b0f792d328af99174d338059f70edfcb7e5a2bf0
Alex Kamsteeg <alex@atlex.nl>
2014-12-07 11:01:17 +0100

    Initial commit, DbContextRepository

---
+++ src/AtleX.Data/Entity/DbContextRepository.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtleX.Data.Entity
+{
+    public abstract class DbContextRepository<T> : IDisposable where T : DbContext
+    {
+        private T _context;
+        protected T Context
+        {
+            get
+            {
+                return _context;
+            }
+        }
+
+        public DbContextRepository(T context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Open a connection to the database
+        /// </summary>
+        public void OpenConnection()
+        {
+            if (Context.Database.Connection.State != ConnectionState.Open)
+            {
+                Context.Database.Connection.Open();
+            }
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+
+            _context.Database.Connection.Close();
+            _context.Dispose();
+            _context = null;
+
+            this.Dispose(true);
+        }
+
+        public virtual void Dispose(bool disposing)
+        {
+        }
+
+        public IQueryable<TDataObject> Query<TDataObject>() where TDataObject : class
+        {
+            return GetObjectSet<TDataObject>();
+        }
+
+        /// <summary>
+        /// Add a new object for saving it to the database
+        /// </summary>
+        /// <typeparam name="TDataObject"></typeparam>
+        /// <param name="objectToAdd"></param>
+        public void Add<TDataObject>(TDataObject objectToAdd) where TDataObject : class
+        {
+            GetObjectSet<TDataObject>().Add(objectToAdd);
+        }
+
+        /// <summary>
+        /// Remove an object
+        /// </summary>
+        /// <typeparam name="TDataObject"></typeparam>
+        /// <param name="objectToDelete"></param>
+        public void Delete<TDataObject>(TDataObject objectToDelete) where TDataObject : class
+        {
+            GetObjectSet<TDataObject>().Remove(objectToDelete);
+        }
+
+        public int SaveChanges()
+        {
+            if (Context.ChangeTracker.HasChanges())
+            {
+                foreach (DbEntityEntry dbObject in Context.ChangeTracker.Entries())
+                {
+                    if (dbObject.Entity is IHasCreated)
+                    {
+                        IHasCreated createdObject = (IHasCreated)dbObject.Entity;
+                        if (createdObject.Created == null)
+                        {
+                            createdObject.Created = DateTime.UtcNow;
+                        }
+                    }
+                    if (dbObject.Entity is IHasLastModified)
+                    {
+                        ((IHasLastModified)dbObject.Entity).LastModified = DateTime.UtcNow;
+                    }
+                }
+            }
+            return Context.SaveChanges();
+        }
+
+        protected virtual DbSet<TDataObject> GetObjectSet<TDataObject>() where TDataObject : class
+        {
+            this.OpenConnection();
+
+            DbSet<TDataObject> result = Context.Set<TDataObject>();
+
+            return result;
+        }
+    }
+}
